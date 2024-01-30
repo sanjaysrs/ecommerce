9311e35144bbe40c297a30ba0e8ef152b11875ee
Sanjay <140075012+sanjaysrs@users.noreply.github.com>
2024-01-30 19:12:29 +0530

    delete CustomUserDetails. update CustomUserDetailsService

--- src/main/java/com/ecommerce/project/service/CustomUserDetailsService.java
+++ src/main/java/com/ecommerce/project/service/CustomUserDetailsService.java
@@ -1,15 +1,18 @@
 package com.ecommerce.project.service;
 
-import com.ecommerce.project.entity.CustomUserDetails;
 import com.ecommerce.project.entity.User;
 import com.ecommerce.project.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -23,22 +26,24 @@ public class CustomUserDetailsService implements UserDetailsService {
 
         Optional<User> userOptional = userRepository.findUserByEmail(username);
 
-        User user;
-        if (userOptional.isPresent()) {
-            user = userOptional.get();
-        } else {
-            user = null;
-        }
+        if (userOptional.isEmpty())
+            throw new RuntimeException("Invalid username");
 
-        if (user == null) {
-            throw new UsernameNotFoundException("Invalid username or password.");
-        }
+        User user = userOptional.get();
+
+        if (!user.isEnabled())
+            throw new DisabledException("User is disabled");
 
         if (!user.isVerified()) {
             throw new DisabledException("User account is not verified");
         }
 
-        return userOptional.map(CustomUserDetails::new).get();
+        String password = user.getPassword();
+
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        user.getRoles().forEach(role->authorities.add(new SimpleGrantedAuthority(role.getName())));
+
+        return new org.springframework.security.core.userdetails.User(username, password, authorities);
 
     }
 
