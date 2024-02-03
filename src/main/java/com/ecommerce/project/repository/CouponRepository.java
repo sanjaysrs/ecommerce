13ec8519ed49b7813a0ecabe12e8633e2bfd8ecf
Sanjay <140075012+sanjaysrs@users.noreply.github.com>
2024-02-03 19:27:30 +0530

    make find coupon case sensitive

--- src/main/java/com/ecommerce/project/repository/CouponRepository.java
+++ src/main/java/com/ecommerce/project/repository/CouponRepository.java
@@ -2,10 +2,13 @@ package com.ecommerce.project.repository;
 
 import com.ecommerce.project.entity.Coupon;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface CouponRepository extends JpaRepository<Coupon, Integer> {
 
-    Optional<Coupon> findCouponByCouponCode(String couponCode);
+    @Query("SELECT c FROM Coupon c WHERE BINARY(c.couponCode) = :couponCode")
+    Optional<Coupon> findCouponByCouponCode(@Param("couponCode") String couponCode);
 }
